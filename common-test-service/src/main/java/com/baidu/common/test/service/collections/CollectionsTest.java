c15e5fd5ca9ac6ce7268102038b79a905415d788
yinhaomin <yinhaomin@baidu.com>
2017-01-11 11:20:12 +0800

    Added collection test

---
+++ common-test-service/src/main/java/com/baidu/common/test/service/collections/CollectionsTest.java
@@ -0,0 +1,14 @@
+package com.baidu.common.test.service.collections;
+
+import java.util.Vector;
+
+public class CollectionsTest {
+
+    public static void main(String[] args) {
+
+        Vector<Integer> vector = new Vector<Integer>();
+        vector.add(4);
+        vector.get(0);
+
+    }
+}
