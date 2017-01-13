7cc772010e1f3fb3078546d1c664ae47c905f779
yinhaomin <yinhaomin@baidu.com>
2017-01-13 17:07:27 +0800

    Added stack and heap test

--- common-test-service/src/main/java/com/baidu/common/test/service/collections/CollectionsTest.java
+++ common-test-service/src/main/java/com/baidu/common/test/service/collections/CollectionsTest.java
@@ -1,5 +1,8 @@
 package com.baidu.common.test.service.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Vector;
 
 public class CollectionsTest {
@@ -9,6 +12,12 @@ public class CollectionsTest {
         Vector<Integer> vector = new Vector<Integer>();
         vector.add(4);
         vector.get(0);
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+
+        List<Integer> list2 = new LinkedList<Integer>();
+        list2.add(1);
 
     }
 }
